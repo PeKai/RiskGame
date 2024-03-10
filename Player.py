78aeb4acd2349851a4d4b1713cdd90b1de45738d
PeKai <1504clark@gmail.com>
2024-03-10 16:50:35 +0000

    Update Player.py

--- Player.py
+++ Player.py
@@ -8,8 +8,8 @@ class Player:
     def __init__(self, country):
         self.my_countries = ["Brazil"]
 
-    def military_placements(self):
-        for country in self.my_countries:
-            if self.world.current_hov == n and pg.mouse.get_pressed()[0]:
-                self.world.countries[country].Infantry += 2
+    def military_placements(self, units):
+        if self.world.current_hov == n and pg.mouse.get_pressed()[0]:
+            for country in self.my_countries:
+                self.world.countries[country].Infantry += units
                 time.sleep(0.3)
