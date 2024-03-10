feec09663a9cb141bea8fc0e285838ded6d07cf0
PeKai <1504clark@gmail.com>
2024-03-10 16:48:07 +0000

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,2 +1,15 @@
+import pygame as pg
+import time
+
+from src import Country, Map
+
+# Work in progress. If it isn't working, just delete Player class!
 class Player:
-    pass
+    def __init__(self, country):
+        self.my_countries = ["Brazil"]
+
+    def military_placements(self):
+        for country in self.my_countries:
+            if self.world.current_hov == n and pg.mouse.get_pressed()[0]:
+                self.world.countries[country].Infantry += 2
+                time.sleep(0.3)
