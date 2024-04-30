d5bb04b5e17c70b37b53598d6e5a23b354baa6c7
PeKai <1504clark@gmail.com>
2024-04-30 02:30:24 +0100

    Update Infantry.py

--- Infantry.py
+++ Infantry.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 
 class Infantry(Enum):
+    """
+    This Infantry Enum is used for the Risk Card, to loop through all the different troop types.
+    """
     Infantry = 1
     Cavalry = 2
     Artillery = 3
