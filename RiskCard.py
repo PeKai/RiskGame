dbb529d37ee2ce68d1c728c899780c5dca091c16
PeKai <1504clark@gmail.com>
2024-04-30 01:57:07 +0100

    Update RiskCard.py

--- RiskCard.py
+++ RiskCard.py
@@ -1,5 +1,8 @@
 class RiskCard:
     def __init__(self, country, infantry):
+        """
+        Instance of a Risk card, where country is of Country instance and infantry is Integer.
+        """
         self.country = country
         self.infantry = infantry
 
