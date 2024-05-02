58bc3cb4dd2d8a88c4b0aee840870d61bc3a9b8e
PeKai <1504clark@gmail.com>
2024-05-02 07:26:09 +0100

    Update Continent.py

--- Continent.py
+++ Continent.py
@@ -2,10 +2,17 @@ from Country import Country
 
 
 class Continent:
-
+    """
+    This instance is not in use, but represents the Continents in the Risk Game. 
+    Plan was to loop through all countries in the Gameplay.py file, create the continent instance, and create a list of continents, similar to 
+    the list of countries in the Gameplay.py class.
+    """
     def __init__(self, countries: list, name):
         self.countries = countries
         self.name = name
 
     def in_Continent(self, country: Country):
+        """
+        Checks if country is in the continent.
+        """
         return self.countries.__contains__(country)
